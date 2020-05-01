3e068f43fa0de5d47877cde905507e45e8a903cc
Ronald Kruizinga <ronmatk@gmail.com>
2020-05-01 12:59:29 +0200

    output start and enddate for cochanges

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ from MBA import perform_mba
 from TimeBetweenCommits import time_between_commits
 from ThresholdAnalysis import threshold_distribution
 from dynamic_Warp import perform_dtw
+from helper_scripts.Commit_date_helper import add_file_dates
 from helper_scripts.output_helper import filter_duplicate_file_pairs, generate_all_pairs
 
 git_url = "https://github.com/SonarSource/sonarlint-intellij.git"
@@ -18,15 +19,17 @@ branch = 'master'
 warps, changedFiles = perform_dtw()
 warpdf = filter_duplicate_file_pairs(warps)
 all_pairs = generate_all_pairs(changedFiles)
-warpdf.to_csv("output/dtw.csv")
+warp_with_dates = add_file_dates(warpdf)
+warp_with_dates.to_csv("output/dtw.csv")
 all_pairs.to_csv("output/file_pairs_dtw.csv")
 
 rules, changedFiles = perform_mba()
 rules = filter_duplicate_file_pairs(rules)
 all_pairs_mba = generate_all_pairs(changedFiles)
+rules_with_dates = add_file_dates(rules)
 # both ante en consequents are sets of length 1
 
-rules.to_csv("output/mba.csv")
+rules_with_dates.to_csv("output/mba.csv")
 all_pairs_mba.to_csv("output/file_pairs_mba.csv")
 
 # Clone the repo we want to analyse.
