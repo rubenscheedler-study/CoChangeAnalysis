10c4b0293258e659963010a154aecca6bf9153f7
ruben <r.j.scheedler@student.rug.nl>
2020-05-29 12:32:46 +0200

    WIP commandline wrapper

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import datetime
+import getopt
+import sys
+
 
 analysis_start_date = datetime.datetime(2015, 4, 12)
 analysis_end_date = datetime.datetime(2020, 5, 14)
@@ -10,4 +13,51 @@ git_repo = "spring-projects/" + project_name
 git_url = "https://github.com/"+git_repo+".git"
 branch = 'master'
 
+git_project_owner = "unknown_owner"  # Only used for the command line initialization
 results_file = "analysis_results.csv"
+
+
+def initialize_config():
+    if __name__ == "__main__":
+        # We are running from the command line. Overwrite the above config attributes with the values below
+        read_args(sys.argv[1:])
+
+
+# Reads arguments from the command line and sets their respective config attributes.
+def read_args(argv):
+    # Parse arguments
+    try:
+        opts, args = getopt.getopt(argv, "p:s:e:o:b:")
+    except getopt.GetoptError:
+        print('<script_name>.py -p <project name> -s <start date dd-mm-yyyy> -e <end date dd-mm-yyyy> -o <owner of the project on git> -b <branch>')
+        sys.exit(2)
+
+    # Store arguments
+    for opt, arg in opts:
+        if opt == '-p':
+            global project_name
+            project_name = arg
+        elif opt == '-s':
+            global analysis_start_date
+            analysis_start_date = datetime.datetime.strptime(arg, '%d-%m-%Y')
+        elif opt == '-e':
+            global analysis_end_date
+            analysis_end_date = datetime.datetime.strptime(arg, '%d-%m-%Y')
+        elif opt == '-o':
+            global git_project_owner
+            git_project_owner = arg
+        elif opt == '-b':
+            global branch
+            branch = arg
+
+    # Update computed properties
+    global input_directory
+    input_directory = "input/" + project_name
+    global output_directory
+    output_directory = "output/" + project_name
+    global clone_directory
+    clone_directory = "projects/" + project_name
+    global git_repo
+    git_repo = git_project_owner + "/" + project_name
+    global git_url
+    git_url = "https://github.com/" + git_repo + ".git"
---
+++ run_exploration.py
@@ -0,0 +1,8 @@
+from Exploration import run_exploration
+from config import initialize_config
+
+# Reads settings from command line
+initialize_config()
+
+# Start the exploration
+run_exploration()
