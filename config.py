16b3d53d4e79bd46458658d830839457cf41acfb
RonaldKruizinga <ronmatk@gmail.com>
2020-07-03 13:22:57 +0200

    Update config to remove disabled and revoked keys

--- config.py
+++ config.py
@@ -13,11 +13,7 @@ git_repo = "SonarSource/" + project_name
 git_url = "https://github.com/"+git_repo+".git"
 branch = 'master'
 
-
-# Ruben 1: c4ca23b80f589f6ea8975ac8a8d1b1d7797f0576
-# Ruben 2: 44adb4aa0b590134e57e6e741992fa124e27d99a
-# Ruben 3: 58979184a7b8b6b40c663e03a84ec920f41233e4
-api_key_github = '3d00e25520031f87d6b41901a7e13ceeebe18dd6' # 'a0b658d0dc9342b2fe5ba236ec4f2d5a29ef85dc'
+api_key_github = 'github_token_here' # 
 git_project_owner = "unknown_owner"  # Only used for the command line initialization
 results_file = "analysis_results.csv"
 
